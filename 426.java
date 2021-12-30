a8cf49f3d11400107371326294c1904beb8a457e
glc6qrx <georgecao2000@gmail.com>
2021-12-30 00:19:42 -0500

    426solution

--- 426.java
+++ 426.java
@@ -37,6 +37,7 @@ class Solution {
         
         if(node.left == null){
             head = node;
+
         }
         
         if(node.left != null){
@@ -120,3 +121,35 @@ class Solution {
                       
                       
 */
+
+
+
+
+
+/* SOLUTION CODE
+class Solution {
+  // the smallest (first) and the largest (last) nodes
+  Node first = null;
+  Node last = null;
+
+  public void helper(Node node) {
+    if (node != null) {
+      // left
+      helper(node.left);
+      // node 
+      if (last != null) {
+        // link the previous node (last)
+        // with the current one (node)
+        last.right = node;
+        node.left = last;
+      }
+      else {
+        // keep the smallest node
+        // to close DLL later on
+        first = node;
+      }
+      last = node;
+      // right
+      helper(node.right);
+    }
+  }
